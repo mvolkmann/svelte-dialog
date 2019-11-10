bb5dba9470f86957d26ce64020bd15171723bcfa
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2019-11-10 16:53:29 -0600

    added a change

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,7 @@
   const openDialog = () => myDialog.showModal();
 </script>
 
+<p>This is a change.</p>
 <button on:click={openDialog}>Open Dialog</button>
 <Dialog bind:dialog={myDialog} title="My Dialog Title">
   My dialog content is very, very long.
